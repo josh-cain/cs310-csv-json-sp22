684c4088119a47d1eceac725b4ec75d0798cfc0f
Josh Cain <josh@joshcain.dev>
2022-02-04 18:35:59 -0600

    Fix Project Scaffolding
    This includes:
     - single-command gradle builds
     - s/wildcard imports/specific imports
     - more standard Java directory structure

--- src/main/java/edu/jsu/mcis/Converter.java
+++ src/main/java/edu/jsu/mcis/Converter.java
@@ -1,17 +1,19 @@
 package edu.jsu.mcis;
 
-import java.io.*;
-import java.util.*;
-import com.opencsv.*;
-import org.json.simple.*;
-import org.json.simple.parser.*;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Iterator;
+import java.util.List;
 
 public class Converter {
-    
+
     /*
-    
+
         Consider the following CSV data:
-        
+
         "ID","Total","Assignment 1","Assignment 2","Exam 1"
         "111278","611","146","128","337"
         "111352","867","227","228","412"
@@ -21,9 +23,9 @@ public class Converter {
         "111160","454","77","125","252"
         "111276","579","130","111","338"
         "111241","973","236","237","500"
-        
+
         The corresponding JSON data would be similar to the following:
-        
+
         {
             "colHeaders":["ID","Total","Assignment 1","Assignment 2","Exam 1"],
             "rowHeaders":["111278","111352","111373","111305","111399","111160",
@@ -38,64 +40,57 @@ public class Converter {
                     [973,236,237,500]
             ]
         }
-    
+
         (Tabs and other whitespace have been added here for clarity.)  Note the
         curly braces, square brackets, and double-quotes!  These indicate which
         values should be encoded as strings, and which values should be encoded
         as integers!  The data files which contain this CSV and JSON data are
         given in the "resources" package of this project.
-    
+
         Your task for this program is to complete the two conversion methods in
         this class, "csvToJson()" and "jsonToCsv()", so that the CSV data shown
         above can be converted to JSON format, and vice-versa.  Both methods
         should return the converted data as strings, but the strings do not need
         to include the newlines and whitespace shown in the examples; again,
         this whitespace has been added only for clarity and readability.
-    
+
         NOTE: YOU SHOULD NOT WRITE ANY CODE WHICH MANUALLY COMPOSES THE OUTPUT
         STRINGS!!!  Leave ALL string conversion to the two data conversion
         libraries we have discussed, OpenCSV and json-simple.  See the "Data
         Exchange" lecture notes for more details, including example code.
-    
+
     */
-    
-    @SuppressWarnings("unchecked")
     public static String csvToJson(String csvString) {
-        
+
         String results = "";
-        
+
         try {
-            
+
             CSVReader reader = new CSVReader(new StringReader(csvString));
             List<String[]> full = reader.readAll();
             Iterator<String[]> iterator = full.iterator();
-            
+
             // INSERT YOUR CODE HERE
-            
-        }        
+
+        }
+
         catch(Exception e) { e.printStackTrace(); }
-        
         return results.trim();
-        
     }
-    
+
     public static String jsonToCsv(String jsonString) {
-        
+
         String results = "";
-        
+
         try {
 
             StringWriter writer = new StringWriter();
             CSVWriter csvWriter = new CSVWriter(writer, ',', '"', '\\', "\n");
-            
-            // INSERT YOUR CODE HERE
-            
-        }
-        
-        catch(Exception e) { e.printStackTrace(); }
-        
-        return results.trim();
-        
-    }
 
+            // INSERT YOUR CODE HERE
+        }
+
+        catch(Exception e) { e.printStackTrace(); }
+        return results.trim();
+    }
 }
--- src/main/java/edu/jsu/mcis/Main.java
+++ src/main/java/edu/jsu/mcis/Main.java
@@ -1,11 +1,14 @@
 package edu.jsu.mcis;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    
+
     /*
-    
+
         This is a simple program for testing the "csvToJson()" and "jsonToCsv()"
         conversion methods in the Converter class.  You should not need to make
         any modifications to this file; instead, complete the conversion methods
@@ -13,63 +16,60 @@ public class Main {
         unit test file to check your work.
 
     */
-    
+
     public static void main(String[] args) {
-        
+
         ClassLoader loader = ClassLoader.getSystemClassLoader();
-        
+
         // Get CSV Data
-        
+
         StringBuilder csvFile = new StringBuilder();
-        
+
         try {
-            
+
             BufferedReader reader = new BufferedReader(new InputStreamReader(loader.getResourceAsStream("resources" + File.separator + "grades.csv")));
             String line;
-            
+
             while((line = reader.readLine()) != null) {
                 csvFile.append(line).append('\n');
             }
-            
+
         }
         catch(IOException e) { e.printStackTrace(); }
-        
+
         String csvFileString = csvFile.toString().trim();
-        
+
         // Get JSON Data
-        
+
         StringBuilder jsonFile = new StringBuilder();
-        
+
         try {
-            
-            BufferedReader reader = new BufferedReader(new InputStreamReader(loader.getResourceAsStream("resources" + File.separator + "grades.json")));            
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(loader.getResourceAsStream("resources" + File.separator + "grades.json")));
             String line;
-            
+
             while((line = reader.readLine()) != null) {
                 jsonFile.append(line).append('\n');
             }
-            
         }
         catch(IOException e) { e.printStackTrace(); }
-        
+
         String jsonFileString = jsonFile.toString().trim();
-        
+
         // Convert CSV to JSON; Print Results to Console
-        
+
         System.out.println("CONVERSION RESULTS (CSV to JSON)");
         System.out.println("================================");
 
         String json = Converter.csvToJson(csvFileString);
         System.out.println(json);
-        
+
         // Convert JSON to CSV; Print Results to Console
-        
+
         System.out.println("\nCONVERSION RESULTS (JSON to CSV)");
         System.out.println("================================");
-        
+
         String csv = Converter.jsonToCsv(jsonFileString);
         System.out.println(csv);
-		
     }
-    
 }
