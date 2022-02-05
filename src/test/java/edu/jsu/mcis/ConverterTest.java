f362cb949b9afafff0dd2c55bf3e5162779de0d8
Josh Cain <josh@joshcain.dev>
2022-02-04 19:55:51 -0600

    More Fixage...
     - Syntactici sugar on assertions
     - Use NIO for file read

--- src/test/java/edu/jsu/mcis/ConverterTest.java
+++ src/test/java/edu/jsu/mcis/ConverterTest.java
@@ -1,102 +1,48 @@
 package edu.jsu.mcis;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.equalTo;
 
 public class ConverterTest {
 
     private String csvString;
     private String jsonString;
 
-    private JSONParser parser;
-    private JSONObject jsonData;
-
     @Before
-    public void setUp() {
-
-        ClassLoader loader = ClassLoader.getSystemClassLoader();
-
-        // Get CSV Data
-
-        StringBuilder csv = new StringBuilder();
-
-        try {
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(loader.getResourceAsStream("grades.csv")));
-
-            String line;
-            while((line = reader.readLine()) != null) {
-                csv.append(line).append('\n');
-            }
-
-        }
-
-        catch(IOException e) { e.printStackTrace(); }
-
-        csvString = csv.toString().trim();
-
-        // Get JSON Data
-
-        parser = new JSONParser();
-
-        StringBuilder json = new StringBuilder();
-
-        try {
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(loader.getResourceAsStream("grades.json")));
-
-            String line;
-            while((line = reader.readLine()) != null) {
-                json.append(line).append('\n');
-            }
-
-        }
-
-        catch(IOException e) { e.printStackTrace(); }
-
-        jsonString = json.toString().trim();
-
-        try {
-
-            jsonData = (JSONObject)parser.parse(jsonString);
-
-        }
-
-        catch(ParseException e) { e.printStackTrace(); }
+    public void setUp() throws IOException {
+        csvString = Files.readString(Path.of(   "src/test/resources/grades.csv")).trim();
+        jsonString = Files.readString(Path.of(   "src/test/resources/grades.json")).trim();
     }
 
     @Test
     public void testConvertCSVtoJSON() {
-        assertEquals(jsonString, Converter.csvToJson(csvString));
+        assertThat(jsonString, is(equalTo(Converter.csvToJson(csvString))));
     }
 
     @Test
     public void testConvertJSONtoCSV() {
-        assertEquals(csvString, Converter.jsonToCsv(jsonString));
+        assertThat(csvString, is(equalTo(Converter.jsonToCsv(jsonString))));
     }
 
     @Test
     public void testConvertJSONtoCSVtoJSON(){
         String csv = Converter.jsonToCsv(jsonString);
         String json = Converter.csvToJson(csv);
-        assertEquals(jsonString, json);
+        assertThat(jsonString, is(equalTo(json)));
     }
 
     @Test
     public void testConvertCSVtoJSONtoCSV(){
         String json = Converter.csvToJson(csvString);
         String csv = Converter.jsonToCsv(json);
-        assertEquals(csvString, csv);
+        assertThat(csvString, is(equalTo(csv)));
     }
-
 }
